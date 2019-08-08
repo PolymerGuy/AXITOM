cc473eb454dd172d5d07e2f7c88b8ff9d296fd3c
Sindre Nordmark Olufsen <sindre.n.olufsen@ntnu.no>
2019-08-08 09:40:11 +0200

    Updated example

--- examples/recon_sim_barrel_axisym.py
+++ examples/recon_sim_barrel_axisym.py
@@ -1,56 +1,56 @@
-from FDK.config import config_from_xtekct
-from FDK.backprojection import fdk
+import FDK as fdk
 import numpy as np
-from FDK.parse import parse_xtekct_file
 from FDK.phantoms import barrel
 import matplotlib.pyplot as plt
 
-
-
+"""
+This example reconstructs a tomogram from a single projection, forward projected using the function fdk.axis_sym_projection.
+The tomogram is then compared to the body that was forward projected.
+"""
 
 
 def main():
-    param = fdk.config_from_xtekct("./example_data/radiogram.xtekct")
-    param.n_voxels_x = 500
-    param.n_voxels_y = 500
-    param.n_voxels_z = 500
-    param.n_pixels_u = 500
-    param.n_pixels_v = 500
-    param.update()
+    config = fdk.config_from_xtekct("./example_data/radiogram.xtekct")
+    config.n_voxels_x = 500
+    config.n_voxels_y = 500
+    config.n_voxels_z = 500
+    config.n_pixels_u = 500
+    config.n_pixels_v = 500
+    config.update()
 
-    volume = barrel(500)
+    radiograms = np.load("proj_barrel.npy")
 
-    angles = np.linspace(0., 45., 200)
-    print(angles)
-#    proj = axis_sym_projection(volume,param,angles=angles)
+    radiograms = np.exp(-radiograms)
 
-#
-    domain = volume
-#
-    #np.save("proj_barrel",proj)
-    proj = np.load("proj_barrel.npy")
+    tomogram = fdk.fdk(radiograms, config)
+
+    return tomogram
 
 
+rec = main().transpose()
 
+correct_norm = barrel(500)[:250, 250, :].transpose()
 
-    proj = np.exp(-proj)
-
-
-    Reconimg = fdk(proj, param)
-
-    return Reconimg, proj, domain
-
-
-rec, proj, domain = main()
-plt.imshow(rec)
 plt.figure()
-# plt.show()
-plt.plot(rec[200,:])
-plt.plot(rec[:,250])
+plt.subplot(1, 3, 1)
+plt.title("Correct")
+plt.imshow(correct_norm, cmap=plt.cm.magma)
+plt.axis('off')
+plt.clim(vmin=0.5, vmax=1.0)
+plt.colorbar()
 
-#plt.plot(rec[2][128,128,:]/rec[2][128,128,128],'--')
-#plt.plot(rec[2][:,128,128]/rec[2][128,128,128],'--')
-plt.figure()
-plt.imshow(np.abs(domain[:250,250,:]-rec))
+plt.subplot(1, 3, 2)
+plt.title("Reconstruction")
+plt.imshow(rec[::-1, :], cmap=plt.cm.magma)
+plt.axis('off')
+plt.clim(vmin=0.5, vmax=1.0)
+plt.colorbar()
 
+plt.subplot(1, 3, 3)
+plt.title("Absolute deviation")
+plt.imshow(np.abs(rec[::-1, :] - correct_norm), cmap=plt.cm.magma)
+plt.axis('off')
+plt.clim(vmin=0, vmax=0.05)
+plt.colorbar()
+plt.tight_layout()
 plt.show()
