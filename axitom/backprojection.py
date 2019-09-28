e54231d9aee5af8ed4ca71ef70b43cec0a90a6e9
Sindre <sindre.n.olufsen@ntnu.no>
2019-09-28 09:05:33 +0200

    Cleaned up docstrings

--- axitom/backprojection.py
+++ axitom/backprojection.py
@@ -11,57 +11,50 @@ This module contains the Feldkamp David Kress filtered back projection routines.
 """
 
 
-def map_object_to_detector_coords(object_xs, object_ys, object_zs, settings):
-    """Map the object coordinates to detector pixel coordinates accounting for cone beam divergence.
-
-        This implementation supports both a axis-symmetric mode and full tomograms.
-        This is determined by the shape of the object_xs array.
-
-        The equations and their derivation is found in:
-        Turbell, H. (2001). Cone-Beam Reconstruction Using Filtered Backprojection. Science And Technology.
-        Retrieved from http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.134.5224&amp;rep=rep1&amp;type=pdf
+def map_object_to_detector_coords(object_xs, object_ys, object_zs, config):
+    """ Map the object coordinates to detector pixel coordinates accounting for cone beam divergence.
 
         Parameters
         ----------
         object_xs : np.ndarray
             The x-coordinate array of the object to be reconstructed
         object_ys : np.ndarray
-            The x-coordinate array of the object to be reconstructed
+            The y-coordinate array of the object to be reconstructed
         object_zs : np.ndarray
-            The x-coordinate array of the object to be reconstructed
-        settings : obj
-            The settings object containing all necessary settings for the reconstruction
+            The z-coordinate array of the object to be reconstructed
+        config : obj
+            The config object containing all necessary settings for the reconstruction
 
 
         Returns
         -------
         detector_cords_a
-            The detector coordinates corresponding to the given points
+            The detector coordinates along the a-axis corresponding to the given points
         detector_cords_b
-            The detector coordinates corresponding to the given points
+            The detector coordinates along the b-axis corresponding to the given points
 
         """
 
-    detector_cords_a = (((object_ys * settings.source_to_detector_dist) / (object_xs + settings.source_to_object_dist)) -
-          settings.detector_us[0]) / settings.pixel_size_u
+    detector_cords_a = (((object_ys * config.source_to_detector_dist) / (object_xs + config.source_to_object_dist)) -
+                        config.detector_us[0]) / config.pixel_size_u
 
     if object_xs.ndim == 2:
-        detector_cords_b = (((object_zs[np.newaxis, np.newaxis, :] * settings.source_to_detector_dist) / (
-                object_xs[:, :, np.newaxis] + settings.source_to_object_dist)) - settings.detector_vs[
-                  0]) / settings.pixel_size_v
+        detector_cords_b = (((object_zs[np.newaxis, np.newaxis, :] * config.source_to_detector_dist) / (
+                object_xs[:, :, np.newaxis] + config.source_to_object_dist)) - config.detector_vs[
+                                0]) / config.pixel_size_v
 
     elif object_xs.ndim == 1:
-        detector_cords_b = (((object_zs[np.newaxis, :] * settings.source_to_detector_dist) / (
-                object_xs[:, np.newaxis] + settings.source_to_object_dist)) - settings.detector_vs[
-                  0]) / settings.pixel_size_v
+        detector_cords_b = (((object_zs[np.newaxis, :] * config.source_to_detector_dist) / (
+                object_xs[:, np.newaxis] + config.source_to_object_dist)) - config.detector_vs[
+                                0]) / config.pixel_size_v
     else:
         raise IOError("Invalid dimensions on the object coordinates")
 
     return detector_cords_a, detector_cords_b
 
 
-def _fdk_axisym(projection_filtered, settings):
-    """Filtered back projection algorithm as proposed by Feldkamp David Kress, adapted for axisymmetry.
+def _fdk_axisym(projection_filtered, config):
+    """ Filtered back projection algorithm as proposed by Feldkamp David Kress, adapted for axisymmetry.
 
         This implementation has been adapted for axis-symmetry by using a single projection only and
         by only reconstructing a single R-Z slice.
@@ -77,16 +70,14 @@ def _fdk_axisym(projection_filtered, settings):
         Parameters
         ----------
         projection_filtered : np.ndarray
-            The ramp filtered projection used in the reconstruction
+            The ramp filtered and weighted projection used in the reconstruction
         config : obj
-            The settings object containing all necessary settings for the reconstruction
+            The config object containing all necessary settings for the reconstruction
 
 
         Returns
         -------
         ndarray
-            The reconstructed slice
-
             The reconstructed slice is a R-Z plane of a axis-symmetric tomogram where Z is the symmetry axis.
 
         """
@@ -97,31 +88,31 @@ def _fdk_axisym(projection_filtered, settings):
     # Allocate an empty array
     recon_slice = np.zeros((proj_center, proj_height), dtype=np.float)
 
-    for frame_nr, angle in enumerate(settings.projection_angs):
+    for frame_nr, angle in enumerate(config.projection_angs):
         print("Back projecting frame nr: %i" % frame_nr)
 
         # Calculate the coordinates of the slice for a given rotation
-        x_rotated, y_rotated = rotate_coordinates(np.zeros_like(settings.object_xs)[:proj_center],
-                                                  settings.object_ys[:proj_center],
+        x_rotated, y_rotated = rotate_coordinates(np.zeros_like(config.object_xs)[:proj_center],
+                                                  config.object_ys[:proj_center],
                                                   np.radians(angle))
 
         # Correct for a center of rotation not being at y=0
-        y_rotated += settings.center_of_rot_y
+        y_rotated += config.center_of_rot_y
 
-        detector_cords_a, detector_cords_b = map_object_to_detector_coords(x_rotated, y_rotated, settings.object_zs,
-                                                                           settings)
+        detector_cords_a, detector_cords_b = map_object_to_detector_coords(x_rotated, y_rotated, config.object_zs,
+                                                                           config)
 
         # a is independent of Z but has to match the shape of b
         detector_cords_a = detector_cords_a[:, np.newaxis] * np.ones_like(detector_cords_b)
 
         # This term is caused by the divergent cone geometry
-        ratio = (settings.source_to_object_dist ** 2.) / (settings.source_to_object_dist + x_rotated) ** 2.
+        ratio = (config.source_to_object_dist ** 2.) / (config.source_to_object_dist + x_rotated) ** 2.
 
         recon_slice = recon_slice + ratio[:, np.newaxis] * map_coordinates(projection_filtered,
                                                                            [detector_cords_a, detector_cords_b],
                                                                            cval=0., order=1)
 
-    return recon_slice / np.float(settings.n_projections)
+    return recon_slice / np.float(config.n_projections)
 
 
 def fdk(projection, config):
@@ -143,14 +134,12 @@ def fdk(projection, config):
         projection : np.ndarray
             The projection used in the reconstruction
         config : obj
-            The settings object containing all necessary settings for the reconstruction
+            The config object containing all necessary settings for the reconstruction
 
 
         Returns
         -------
         ndarray
-            The reconstructed slice
-
             The reconstructed slice is a R-Z plane of a axis-symmetric tomogram where Z is the symmetry axis.
 
         """
@@ -161,7 +150,6 @@ def fdk(projection, config):
     if type(projection) != np.ndarray:
         raise IOError("The projections have to be in a numpy ndarray")
 
-
     projection = -np.log(projection)
 
     if projection.ndim == 2:
@@ -170,6 +158,5 @@ def fdk(projection, config):
     else:
         raise IOError("The projection has to be a 2D array")
 
-
-    img = _fdk_axisym(projection_filtered, config)
-    return img
+    tomo = _fdk_axisym(projection_filtered, config)
+    return tomo
